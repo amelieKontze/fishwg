8e5c219c3f2939273bf7d97a7d1288d580fb147c
amelie <amelieka@gmx.de>
2023-06-13 16:36:23 +0200

    add service test

--- backend/src/test/java/de/neuefische/service/FishServiceTest.java
+++ backend/src/test/java/de/neuefische/service/FishServiceTest.java
@@ -4,8 +4,11 @@ import de.neuefische.model.Fish;
 import de.neuefische.repository.FishRepo;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class FishServiceTest {
 
@@ -26,6 +29,13 @@ class FishServiceTest {
     FishService fishService = new FishService(fishRepo);
 
     @Test
-    void getAllFish() {
+    void getAllFish_shouldReturnAListOfAllFish() {
+        //given
+        when(fishRepo.findAll()).thenReturn(List.of(fish));
+        //when
+        List<Fish> actual = fishService.getAllFish();
+        //then
+        assertEquals(actual, List.of(fish));
+
     }
 }
