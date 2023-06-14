226d8942ff09700c6fa3e82afe8794ebee6ed10e
amelie <amelieka@gmx.de>
2023-06-14 13:50:44 +0200

    update FishCard.css

--- frontend/src/components/FishCard.tsx
+++ frontend/src/components/FishCard.tsx
@@ -9,16 +9,18 @@ type Props = {
 function FishCard(props:Props) {
     return (
         <div className="fish-card">
-            <div className="fish-card-front">
-            <h2>{props.fish.name}</h2>
-            <img className="fish-pic" src={props.fish.image} alt={"picture of fish"}/>
-            </div>
-            <div className="fish-card-back">
-                <p>{props.fish.waterType}</p>
-                <p>{props.fish.origin}</p>
-                <p>{props.fish.temperatureInCelsius}</p>
-                <p>{props.fish.pH}</p>
-                <p>{props.fish.minTankSizeInLitres}liter</p>
+            <div className="fish-card-inner">
+                <div className="fish-card-front">
+                    <h2>{props.fish.name}</h2>
+                    <img className="fish-pic" src={props.fish.image} alt={"picture of fish"}/>
+                </div>
+                <div className="fish-card-back">
+                    <p>{props.fish.waterType}</p>
+                    <p>{props.fish.origin}</p>
+                    <p>{props.fish.temperatureInCelsius}</p>
+                    <p>{props.fish.pH}</p>
+                    <p>{props.fish.minTankSizeInLitres}liter</p>
+                </div>
             </div>
         </div>
     );
