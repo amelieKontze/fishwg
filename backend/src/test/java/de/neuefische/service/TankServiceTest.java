779beb5dc002a16478f14d264dd20e56a2c3f5c1
amelie <amelieka@gmx.de>
2023-06-21 18:19:32 +0200

    update TankServiceTest

--- backend/src/test/java/de/neuefische/service/TankServiceTest.java
+++ backend/src/test/java/de/neuefische/service/TankServiceTest.java
@@ -1,5 +1,6 @@
 package de.neuefische.service;
 
+import de.neuefische.model.Fish;
 import de.neuefische.model.Tank;
 import de.neuefische.repository.TankRepo;
 import org.junit.jupiter.api.Test;
@@ -14,12 +15,28 @@ class TankServiceTest {
 
     TankRepo tankRepo = mock(TankRepo.class);
 
+    Fish fish = new Fish(
+            "1",
+            "Betta",
+            "Betta Splendens",
+            "",
+            "Süßwasser",
+            "Thailand",
+            20,
+            25,
+            30,
+            6.5,
+            8.0,
+            "aggressiv");
+    List<Fish> residentFish = List.of(fish);
+
     Tank tank = new Tank(
             "1",
             "30l Nano",
             "Süßwasser",
             30,
             27,
+            residentFish,
             7.5
     );
 
