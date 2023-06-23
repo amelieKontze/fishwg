6573883589279061aa70b41ef1c043d97f9d8bc0
amelie <amelieka@gmx.de>
2023-06-23 14:30:39 +0200

    put useFish in App.tsx

--- frontend/src/pages/FindFish.tsx
+++ frontend/src/pages/FindFish.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {Fish} from "../model/FishModel";
 import FishCard from "../components/FishCard";
 
@@ -16,16 +16,84 @@ function FindFish(props: Props) {
     const [filterPh, setFilterPh] = useState<number>(0)
     const [filterTemperament, setFilterTemperament] = useState<string>("")
 
-    useEffect(() => {
-        props.getAllFish()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
 
+    const filteredFish = props.allFish.filter((fish) => {
+        const nameMatch = fish.name.toLowerCase().includes(filterName.toLowerCase());
+        const waterTypeMatch = fish.waterType.toLowerCase() === filterWaterType.toLowerCase();
+        const tankSizeMatch = fish.minTankSizeInLitres <= filterTankSize;
+        return nameMatch && waterTypeMatch && tankSizeMatch;
+    });
+
+    function onChangeHandlerSetFilterName(e: ChangeEvent<HTMLInputElement>) {
+        setFilterName(e.target.value)
+    }
+
+    function onChangeHandlerSetFilterWaterType(e: ChangeEvent<HTMLInputElement>) {
+        setFilterWaterType(e.target.value)
+    }
+
+    function onChangeHandlerSetFilterTankSize(e: ChangeEvent<HTMLInputElement>) {
+        const value = Number(e.target.value);
+        setFilterTankSize(value);
+    }
+
+    function onChangeHandlerSetFilterOrigin(e: ChangeEvent<HTMLInputElement>) {
+        setFilterOrigin(e.target.value)
+    }
 
     return (
         <>
             <h1>Find Fish</h1>
-            {props.allFish.map(fish => <FishCard key={fish.id} fish={fish}/>)}
+
+            <input placeholder="Search fish by name" className="input-text-field"
+                   onChange={onChangeHandlerSetFilterName}/>
+            <div className="radio-buttons">
+                <label className="radio-button">
+                    <input name="option-water-type" type="radio" value="Süßwasser"
+                           checked={filterWaterType === "Süßwasser"}
+                           onChange={onChangeHandlerSetFilterWaterType}/>
+                    <div className="radio-circle"></div>
+                    <span className="radio-label">Süßwasser</span>
+                </label>
+                <label className="radio-button">
+                    <input name="option-water-type" type="radio" value="Salzwasser"
+                           checked={filterWaterType === "Salzwasser"}
+                           onChange={onChangeHandlerSetFilterWaterType}/>
+                    <div className="radio-circle"></div>
+                    <span className="radio-label">Salzwasser</span>
+                </label>
+            </div>
+            <div>
+                <input className="input-number" type="number" placeholder="Tank size in litres"
+                       value={filterTankSize !== 0 ? filterTankSize : ""} onChange={onChangeHandlerSetFilterTankSize}/>
+            </div>
+            <div className="radio-buttons">
+                <label className="radio-button">
+                    <input name="option-origin" type="radio" value="Asien" checked={filterOrigin === "Asien"}
+                           onChange={onChangeHandlerSetFilterWaterType}/>
+                    <div className="radio-circle"></div>
+                    <span className="radio-label">Asien</span>
+                </label>
+                <label className="radio-button">
+                    <input name="option-origin" type="radio" value="Amerika" checked={filterOrigin === "Amerika"}
+                           onChange={onChangeHandlerSetFilterWaterType}/>
+                    <div className="radio-circle"></div>
+                    <span className="radio-label">Amerika</span>
+                </label>
+            </div>
+
+
+            {filteredFish.length > 0 && (
+                <div className="gallery">
+                    {filteredFish.map(fish => <FishCard key={fish.id} fish={fish}/>)}
+                </div>
+            )}
+
+            {filteredFish.length === 0 && props.allFish.length > 0 && (
+                <div className="gallery">
+                    {props.allFish.map(fish => <FishCard key={fish.id} fish={fish}/>)}
+                </div>
+            )}
         </>
     );
 }
