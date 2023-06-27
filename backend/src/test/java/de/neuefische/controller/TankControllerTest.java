88c6f2382307d0a981670c4da48261c50d0293cb
amelie <amelieka@gmx.de>
2023-06-27 12:47:47 +0200

    add deleteTank TankControllerTest

--- backend/src/test/java/de/neuefische/controller/TankControllerTest.java
+++ backend/src/test/java/de/neuefische/controller/TankControllerTest.java
@@ -1,10 +1,13 @@
 package de.neuefische.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.neuefische.model.Tank;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -48,5 +51,35 @@ class TankControllerTest {
                               "residentFish": [] 
                             }
                         """)).andExpect(jsonPath("$.id").isNotEmpty());
+    }
+
+    @Test
+    void deleteTank_shouldRemoveTankFromDB() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/tank/new-tank")
+                        .contentType("application/json")
+                        .content("""
+                                    {
+                                     "name": "nano",
+                                     "waterType": "Süßwasser",
+                                     "tankSizeInLitres": 45,
+                                     "tankTemperature": 23,
+                                     "tankPh": 6.7 , 
+                                     "residentFish": []
+                                   }
+                                """))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Tank tank = objectMapper.readValue(content, Tank.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/tank/delete/" + tank.getId()))
+                .andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/tank/my-tanks"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+
     }
 }
