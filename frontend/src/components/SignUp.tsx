0e581838654c5dfc322835f895c2c486d6cc9c75
amelie <amelieka@gmx.de>
2023-07-10 19:18:06 +0200

    add errors for empty input in frontend

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,5 +1,6 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import {Link} from "react-router-dom";
+import "../stylesheets/SignUp.css"
 
 type Props = {
     signUp: (name: string, username: string, email: string, password: string) => Promise<void>
@@ -11,6 +12,7 @@ function SignUp(props: Props) {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState('');
 
     const onChangeHandlerSetName = (e: ChangeEvent<HTMLInputElement>) => {
         setName(e.target.value);
@@ -28,30 +30,55 @@ function SignUp(props: Props) {
         setPassword(e.target.value);
     };
 
+    const onChangeHandlerSignUp = (e: FormEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
-    function onChangeHandlerSignUp() {
-        props.signUp(name, username, email, password)
-    }
+        if (name.trim() === '') {
+            setError('Name must not be empty.');
+            return;
+        }
+
+        if (username.trim() === '') {
+            setError('Username must not be empty.');
+            return;
+        }
+
+        if (!emailRegex.test(email)) {
+            setError('Please provide a valid email.');
+            return;
+        }
+
+        if (password.length < 8) {
+            setError('Password should be at least 8 characters long.');
+            return;
+        }
+
+        setError('');
+        props.signUp(name, username, email, password);
+    };
+
 
     return (
         <div className="login">
             <h1>Sign Up</h1>
-            <div>
+            <form>
                 <input className="input-text-field" placeholder={"Name"} type="text" value={name}
-                       onChange={onChangeHandlerSetName}/>
+                       onChange={onChangeHandlerSetName} required/>
                 <br/>
                 <input className="input-text-field" placeholder={"Username"} type="text" value={username}
-                       onChange={onChangeHandlerSetUsername}/>
+                       onChange={onChangeHandlerSetUsername} required/>
                 <br/>
-                <input className="input-text-field" placeholder={"Email"} type="text" value={email}
-                       onChange={onChangeHandlerSetEmail}/>
+                <input className="input-text-field" placeholder={"Email"} type="email" value={email}
+                       onChange={onChangeHandlerSetEmail} required/>
                 <br/>
                 <input className="input-text-field" placeholder={"Password"} type="password" value={password}
-                       onChange={onChangeHandlerSetPassword}/>
+                       onChange={onChangeHandlerSetPassword} required/>
+                {error && <p className="error-message">{error}</p>}
                 <button className="button" onClick={onChangeHandlerSignUp}>Sign up</button>
                 <br/>
                 <Link to="/login" className="link">Login</Link>
-            </div>
+            </form>
         </div>
     );
 }
